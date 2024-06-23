aa21440a65b3416ef88a0a9bee3c85c42888029f
Rodrigo Silva <rodrigo.amancio@youse.com.br>
2024-06-22 23:22:38 -0300

    Initial Commit

---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by Rodrigo Silva on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weather_appTests/weather_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appTests.swift
+//  weather_appTests
+//
+//  Created by Rodrigo Silva on 22/06/24.
+//
+
+import Testing
+@testable import weather_app
+
+struct weather_appTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
